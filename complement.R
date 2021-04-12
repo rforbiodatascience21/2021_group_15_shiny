9ba4d5f57d5560d6534a53c3f81b8cebf01641f4
MieGAnd <s174343@student.dtu.dk>
2021-04-12 07:55:21 +0000

    Complement file

---
+++ complement.R
@@ -0,0 +1,6 @@
+complement <- function(dna){
+  lookup <- c("A" = "T", "T" = "A", "G" = "C", "C" = "G") # named vector, think key/value
+  dna_split <- strsplit(dna, "")[[1]]
+  dna_complement <- paste0(lookup[dna_split], collapse = "")
+  return(dna_complement)
+}
